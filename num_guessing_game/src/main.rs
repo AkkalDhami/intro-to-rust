8f2982dd7c694c0778719d69ba4f769b2d7fe538
AkkalDhami <dhamiakkal21@gmail.com>
2026-09-15 08:56:38 +0545

    add functionality to generate and display a secret number in the guessing game

--- num_guessing_game/src/main.rs
+++ num_guessing_game/src/main.rs
@@ -1,8 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
     println!("Please input your guess: ");
 
     let mut guess: String = String::new();
@@ -10,6 +14,8 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: i32 = guess.trim().parse().expect("Failed to parse");
 
     println!("You guessed: {guess}");
 }
